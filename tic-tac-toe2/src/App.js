1cd93dc186113e8a1262462d8f9f1ea567b15dc9
jihoo <ahrefhoo@naver.com>
2024-02-12 23:30:43 +0900

    리액트문서 심화기능2 완료

--- tic-tac-toe2/src/App.js
+++ tic-tac-toe2/src/App.js
@@ -38,24 +38,25 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next Player: " + (xIsNext ? "X" : "O");
   }
 
+  const squareGroup = (startIdx) => {
+    let three_squares = [startIdx, startIdx + 1, startIdx + 2].map((index) => {
+      return (
+        <Square
+          value={squares[index]}
+          onSquareClick={() => handleClick(index)}
+        />
+      );
+    });
+
+    //console.log(three_squares);
+    return three_squares;
+  };
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <div className="board-row">{squareGroup(0)}</div>
+      <div className="board-row">{squareGroup(3)}</div>
+      <div className="board-row">{squareGroup(6)}</div>
     </>
   );
 }
