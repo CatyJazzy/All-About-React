e448492208c1975ed802a434e23374acb8b5581e
jihoo <ahrefhoo@naver.com>
2024-02-13 00:08:41 +0900

    리액트문서 심화기능3 완료

--- tic-tac-toe2/src/App.js
+++ tic-tac-toe2/src/App.js
@@ -64,6 +64,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0); //사용자가 보고 있는 보드 순번
+  const [isAscend, setAscend] = useState(true); //히스토리 정렬기준
 
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove]; //현재 보드 상태
@@ -80,7 +81,12 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
+  function handleList() {
+    setAscend(!isAscend);
+  }
+
   /* 히스토리 목록 엘리먼트 */
+
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
@@ -106,7 +112,10 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <button onClick={handleList}>
+          {isAscend ? "내림차순으로" : "오름차순으로"}
+        </button>
+        <ol>{isAscend ? moves : [...moves].reverse()}</ol>
       </div>
     </div>
   );
