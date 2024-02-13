2846eb086416b02000c4647a020a8c152cedbc02
jihoo <ahrefhoo@naver.com>
2024-02-13 21:15:20 +0900

    리액트문서 심화기능4 - 목차에서 이동시 승리줄 업데이트 오류

--- tic-tac-toe2/src/App.js
+++ tic-tac-toe2/src/App.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+function Square({ value, onSquareClick, isWin }) {
+  console.log(isWin, "--네모 인덱스: ", value);
+
+  if (isWin) {
+    return (
+      <button
+        className="square"
+        onClick={onSquareClick}
+        style={{ backgroundColor: "yellow" }}
+      >
+        {value}
+      </button>
+    );
+  } else {
+    return (
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    );
+  }
 }
 
 function Board({ xIsNext, squares, onPlay }) {
   /* 칸에 값 채우는 함수 */
   function handleClick(i) {
     /* 이미 채워진 칸인지 확인 && 승리 확인 */
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i] || calculateWinner(squares)[0]) {
       return;
     }
 
@@ -29,21 +43,33 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   /* 승자 및 순서 표시 */
-  const winner = calculateWinner(squares);
+  const winner = calculateWinner(squares)[0];
   let status; // 순서 및 결과
+  let win_status; // 이긴 줄;
 
   if (winner) {
     status = "Winner: " + winner;
+    win_status = calculateWinner(squares).slice(1, 4);
   } else {
     status = "Next Player: " + (xIsNext ? "X" : "O");
+    win_status = [null, null, null];
   }
 
   const squareGroup = (startIdx) => {
     let three_squares = [startIdx, startIdx + 1, startIdx + 2].map((index) => {
+      let isWinSquare = false;
+
+      for (let i = 0; i < 3; i++) {
+        if (startIdx === win_status[i]) {
+          isWinSquare = true;
+        }
+      }
+
       return (
         <Square
           value={squares[index]}
           onSquareClick={() => handleClick(index)}
+          isWin={isWinSquare}
         />
       );
     });
@@ -137,9 +163,9 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], a, b, c];
     }
   }
 
-  return null;
+  return [null];
 }
