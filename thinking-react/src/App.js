7e3e55032de03676a46520bb9a6b76d6e3cd0e66
jihoo <ahrefhoo@naver.com>
2024-03-02 13:02:37 +0900

    Thinking in React STEP3 STEP4

--- thinking-react/src/App.js
+++ thinking-react/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ProductCateoryRow({ category }) {
   return (
     <tr>
@@ -50,22 +52,30 @@ function ProductTable({ products }) {
   );
 }
 
-function SearchBar() {
+function SearchBar({ filterText, inStockOnly }) {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input type="text" value={filterText} placeholder="Search..." />
       <label>
-        <input type="checkbox" /> Only show products in stock
+        <input type="checkbox" checked={inStockOnly} /> Only show products in
+        stock
       </label>
     </form>
   );
 }
 
 function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly} />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
     </div>
   );
 }
